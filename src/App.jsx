4a8da5bc6913e3d30a17c865d16e12e13cceea0e
iamfaizan-1 <faizanfaizy634@gmail.com>
2026-06-08 20:41:29 +0500

    tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import MarginTest from './Components/MarginTest'
+
+function App() {
+  return (
+    <div className=' h-100'  >
+
+
+{/* <div className="size-32 bg-gray-700 relative rounded-md mb-3">
+  <div className="w-full h-[40px] bg-amber-500 rounded-md absolute z-[10] ">
+
+  </div>
+</div>
+
+<div className="grid lg:grid-cols-3   md:grid-cols-2 gap-4">
+<div className="bg-amber-600 p-3">box1</div>
+<div className="bg-amber-600 p-3  ">box2</div>
+<div className="bg-amber-600 p-3" >box3</div>
+</div>
+ */}
+
+      {/* <h1 className='text-center'>Hello world!</h1>
+      <h1>faizan</h1>
+      <h1 className="float-end" >rehman</h1> */}
+
+<MarginTest/>
+
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/MarginTest.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function MarginTest() {
+  return (
+    <div>
+<div className="box1 h-20 w-20 bg-amber-400 mb-1.5 "></div>
+<div className="box2 h-20 w-20 bg-amber-600"></div>
+
+    </div>
+    
+  )
+}
+
+export default MarginTest
